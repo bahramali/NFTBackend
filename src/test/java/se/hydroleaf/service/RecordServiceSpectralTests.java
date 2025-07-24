287dafa3541cb3c37ac8add10821262d2d36f47f
Azad Bahramali <bahramali.az@gmail.com>
2025-07-25 00:17:44 +0200

    revert RecordService

--- src/test/java/se/hydroleaf/service/RecordServiceSpectralTests.java
+++ src/test/java/se/hydroleaf/service/RecordServiceSpectralTests.java
@@ -24,38 +24,5 @@ class RecordServiceSpectralTests {
     @Autowired
     private SensorRecordRepository recordRepository;
 
-    @Test
-    void spectralDataIsSplitIntoFourSensors() {
-        String json = """
-            {
-              \"deviceId\": \"dev1\",
-              \"location\": \"loc\",
-              \"timestamp\": \"2024-01-01T00:00:00Z\",
-              \"sensors\": [
-                {
-                  \"sensorId\": \"spec1\",
-                  \"type\": \"veml7700\",
-                  \"unit\": \"lux\",
-                  \"value\": {
-                    \"415nm\":1,\"445nm\":2,\"480nm\":3,\"515nm\":4,
-                    \"555nm\":5,\"590nm\":6,\"630nm\":7,\"680nm\":8,
-                    \"clear\":9,\"nir\":10
-                  }
-                }
-              ]
-            }
-            """;
 
-        recordService.saveMessage("growSensors", json);
-
-        List<SensorRecord> records = recordRepository.findAll();
-        assertEquals(1, records.size());
-        List<SensorData> sensors = records.get(0).getSensors();
-        assertEquals(4, sensors.size());
-        Set<String> types = sensors.stream().map(SensorData::getType).collect(Collectors.toSet());
-        assertTrue(types.contains("blueLight"));
-        assertTrue(types.contains("redLight"));
-        assertTrue(types.contains("clear"));
-        assertTrue(types.contains("nir"));
-    }
 }

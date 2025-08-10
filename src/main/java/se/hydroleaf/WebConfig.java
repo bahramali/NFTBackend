a06aeb2484f4a2b502930ad4608838f0634848a4
Azad <bahramali.az@gmail.com>
2025-08-10 12:20:44 +0200

    Update WebConfig.java

--- src/main/java/se/hydroleaf/WebConfig.java
+++ src/main/java/se/hydroleaf/WebConfig.java
@@ -3,7 +3,7 @@ package se.hydroleaf;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 

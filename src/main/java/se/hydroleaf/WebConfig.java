5a302d7698f941b88b695014d623e8e648290275
Azad <bahramali.az@gmail.com>
2025-08-10 12:01:05 +0200

    feat: add SPA fallback routing

--- src/main/java/se/hydroleaf/WebConfig.java
+++ src/main/java/se/hydroleaf/WebConfig.java
@@ -2,6 +2,7 @@ package se.hydroleaf;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,5 +15,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{path:^(?!api$)[^\\.]*}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/{path:^(?!api$)[^\\.]*}/**/{subpath:[^\\.]*}")
+                .setViewName("forward:/index.html");
     }
 }

12d4016bedbd038f7068b5c339af93a36e0d4362
Azad <bahramali.az@gmail.com>
2025-08-10 12:19:12 +0200

    Update WebConfig.java

--- src/main/java/se/hydroleaf/WebConfig.java
+++ src/main/java/se/hydroleaf/WebConfig.java
@@ -7,6 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+      @Override
+      public void addViewControllers(ViewControllerRegistry registry) {
+        // Any single-segment track without a dot
+        registry.addViewController("/{path:[^\\.]*}")
+                .setViewName("forward:/index.html");
+    
+        // Any multi-segment path whose last segment has no dots
+        registry.addViewController("/**/{path:[^\\.]*}")
+                .setViewName("forward:/index.html");
+      }
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")

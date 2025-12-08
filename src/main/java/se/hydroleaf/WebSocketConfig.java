bd20a7e129ad801c591a733623896d66b1da6495
Azad <bahramali.az@gmail.com>
2025-12-09 00:20:48 +0100

    Add configurable CORS and websocket origins

--- src/main/java/se/hydroleaf/WebSocketConfig.java
+++ src/main/java/se/hydroleaf/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package se.hydroleaf;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,10 +11,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final String[] allowedOrigins;
+
+    public WebSocketConfig(@Value("${cors.allowed-origins:https://hydroleaf.app}") String[] allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOrigins(allowedOrigins);
     }
 
     @Override

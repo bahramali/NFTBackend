1dc6e15a1459682e1715395804758d5994f8de1d
Azad <bahramali.az@gmail.com>
2025-12-19 14:07:37 +0100

    Return JSON errors from API advice

--- src/main/java/se/hydroleaf/controller/ApiExceptionHandler.java
+++ src/main/java/se/hydroleaf/controller/ApiExceptionHandler.java
@@ -12,12 +12,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-@ControllerAdvice
+@RestControllerAdvice
 @org.springframework.core.annotation.Order(org.springframework.core.Ordered.HIGHEST_PRECEDENCE)
 public class ApiExceptionHandler {
 

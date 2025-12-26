9dce95b4854bd24259a07dad579acc4d3856d398
Azad <bahramali.az@gmail.com>
2025-12-26 16:48:41 +0100

    Adjust Flyway IT to use legacy schema seed

--- src/main/resources/db/migration/V1__add_user_notification_flags.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE app_user
-    ADD COLUMN order_confirmation_emails boolean NOT NULL DEFAULT true;
-ALTER TABLE app_user
-    ADD COLUMN pickup_ready_notification boolean NOT NULL DEFAULT true;
-
-ALTER TABLE app_user
-    ALTER COLUMN order_confirmation_emails DROP DEFAULT;
-ALTER TABLE app_user
-    ALTER COLUMN pickup_ready_notification DROP DEFAULT;
